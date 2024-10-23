ec17d44489b71be18be2bbd42c111104bba0dedf
Berkay Karabulut <101340187+brkykb@users.noreply.github.com>
2024-10-23 16:41:02 +0300

    Add files via upload

---
+++ HesapMakinesi/HesapMakinesi/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string username = "Berkay";
+        int password = 1234;
+        int counter = 0;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username == textBox1.Text && password == Convert.ToInt32(textBox2.Text))
+            {
+                MessageBox.Show("Giris Basarili");
+                Form1 form1 = new Form1();
+                this.Hide();
+
+
+                Form2 form = new Form2();
+                form.Show();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Kullanici Adi veya Sifre Hatali");
+                counter++;
+                if (counter == 3)
+                {
+                    MessageBox.Show("3 Defa Yanlis Giris Yaptiniz.");
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HesapMakinesi/HesapMakinesi/Form2.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HesapMakinesi
+{
+    public partial class Form2 : Form
+    {
+        char _proces_type;
+        bool _clear_screen;
+        int _first_number;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Screen_Label.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "2";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "3";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "5";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "6";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "8";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "9";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int second_number = Convert.ToInt16(Screen_Label.Text);
+            double result;
+
+            switch (_proces_type)
+            {
+                case '+':
+                    result = _first_number + second_number;
+                    break;
+                case '-':
+                    result = _first_number - second_number;
+                    break;
+                case '*':
+                    result = _first_number * second_number;
+                    break;
+                case '/':
+                    result = _first_number / second_number;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            Screen_Label.Text = Convert.ToString(result);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            _proces_type = '/';
+            _clear_screen = true;
+            _first_number = Convert.ToInt32(Screen_Label.Text);
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            _proces_type = '*';
+            _clear_screen = true;
+            _first_number = Convert.ToInt32(Screen_Label.Text);
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            _proces_type = '-';
+            _clear_screen = true;
+            _first_number = Convert.ToInt32(Screen_Label.Text);
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            _proces_type = '+';
+            _clear_screen = true;
+            _first_number = Convert.ToInt32(Screen_Label.Text);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_clear_screen == true)
+            {
+                Screen_Label.Text = "";
+                _clear_screen = false;
+            }
+            if (Screen_Label.Text == "0") Screen_Label.Text = "";
+            Screen_Label.Text += "1";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
